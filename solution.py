7cb09e458c6e5c11436a03a5fe23f247f13565e6
Yash Maurya <59893283+yashmaurya01@users.noreply.github.com>
2020-05-06 17:42:13 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,13 @@
+def solution(n):
+    p = ""
+    for i in range(2,30000):
+        x = 0
+        for j in range(2,i/2+1):
+            if i%j==0:
+                x = 1
+                break
+            
+        if x == 0:
+            p+=str(i)
+            
+    print p[n:n+5]
